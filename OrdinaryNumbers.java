3f04c8f0f78c70c3a48c5e04c5c705e9a2cc2966
shobhit2122verma <44787115+shobhit2122verma@users.noreply.github.com>
2021-07-22 23:35:18 +0530

    revision tree continued in treemain.java class

--- OrdinaryNumbers.java
+++ OrdinaryNumbers.java
@@ -1,5 +1,31 @@
 package Arrays;
 
-public class OrdinaryNumbers {
+import java.util.Scanner;
 
+public class OrdinaryNumbers {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t!=0) {
+			int n=in.nextInt();
+			int ans=0;
+			if(n>0&&n<=9) {
+				ans=n;
+			}
+			else
+			{
+				if(n>9) {
+					for(int i=1;i<=9;i++) {
+						long k=i;
+						while(k<=n) {
+							ans=ans+1;
+							k=(k*10)+i;
+						}
+					}
+				}
+			}
+			System.out.println(ans);
+			t--;
+		}
+	}
 }
--- Treemain.java
+++ Treemain.java
@@ -21,7 +21,7 @@ public class Treemain {
 	
 	public static TreeNode<Integer> leveltakeInput(Scanner in)throws NoSuchElementException{
 		int n=in.nextInt();
-		if(n==-1) {
+		if(n==0) {
 			return null;
 		}
 		TreeNode<Integer> root=new TreeNode<Integer>(n);
@@ -29,7 +29,7 @@ public class Treemain {
 		while(!queue.isEmpty()) {
 			TreeNode<Integer> root1=queue.peek();
 //			ArrayList<TreeNode> childrens=root1.children;
-			System.out.println("enter the number of childrens for "+root1.data);
+//			System.out.println("enter the number of childrens for "+root1.data);
 			int children=in.nextInt();
 			for(int i=0;i<children;i++) {
 				int data=in.nextInt();
@@ -60,9 +60,36 @@ public class Treemain {
 			}
 			queue.remove();		}
 	}
+	
+	public static int findHeight(TreeNode<Integer> root) {
+		if(root==null) {
+			return 1;
+		}
+		int ans=1;
+		for(int i=0;i<root.children.size();i++) {
+			int data=1+findHeight(root.children.get(i));
+			ans= Math.max(ans, data);
+		}
+		return ans;
+	}
+	public static int countleafnodes(TreeNode<Integer> root) {
+		if(root==null) {
+			return 0;
+		}
+		if(root.children.size()==0) {
+			return 1;
+		}
+		int ans=0;
+		for(int i=0;i<root.children.size();i++) {
+			ans=ans+countleafnodes(root.children.get(i));
+		}
+		return ans;
+	}
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		TreeNode<Integer> root=leveltakeInput(in);
-		printLevel(root);
+//		printLevel(root);
+//		System.out.println(findHeight(root));
+		System.out.println(countleafnodes(root));
 	}
 }
